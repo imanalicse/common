f85b1713d55ffb3516855adb3b21854b4b4cf8f5
iman <iman@bitmascot.com>
2017-03-22 16:44:21 +0600

    afas

---
+++ angularjs/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html ng-app="">
+
+<head>
+    <script src = "js/angular.min.js"></script>
+    <script src = "js/script.js"></script>
+</head>
+
+
+<body>
+<div ng-controller="GreetingController">
+    {{ greeting }}
+</div>
+
+</body>
+</html>
--- index.php
+++
@@ -1,5 +0,0 @@
-Hello world
-
-This is test
-
-Hello

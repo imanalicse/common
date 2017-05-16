e72d44119420e683521a1e08d57565b4c19d522a
iman <iman@bitmascot.com>
2017-05-16 16:41:47 +0600

    css add

--- gulp/work/gulpfile.js
+++ gulp/work/gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
+var autoprefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
 
 gulp.task('imagemin', function() {
     var img_src = 'src/images/**/*';
@@ -9,4 +12,12 @@ gulp.task('imagemin', function() {
         //.pipe(changed(img_dest))
         .pipe(imagemin())
         .pipe(gulp.dest(img_dest));
+});
+
+gulp.task('styles', function() {
+    gulp.src(['src/styles/*.css'])
+        .pipe(concat('styles.css'))
+        .pipe(autoprefix('last 2 versions'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('build/styles/'));
 });
